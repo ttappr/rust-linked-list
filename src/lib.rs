cb36149bcbed2c26a452b8fd8ee4b3fb26b9dc83
ttappr <tmtappr@gmail.com>
2021-10-07 15:43:00 -0700

    Debugged and working.

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,8 @@ impl<T> LinkedList<T> {
     fn extract_value(self) -> T {
         match self {
             Filled(bx) => bx.value,
-            Empty => panic!("Attempt to extract value from Empty Node."),
+            Empty => panic!("Attempt to extract value 
+                             from Empty Node."),
         }
     }
     pub fn get(&self, index: usize) -> Option<&T> {
@@ -83,13 +84,15 @@ impl<T> LinkedList<T> {
         *curr = Self::from_value(value);
     }
     pub fn pop_front(&mut self) -> Option<T> {
-        let ret;
-        if !self.is_empty() && !self.next.is_empty() {
-            let next = self.next.take();
-            ret      = Some(self.take().extract_value());
-            *self    = next;
-        } else {
-            ret = Some(self.take().extract_value());
+        let mut ret = None;
+        if !self.is_empty() {
+            if !self.next.is_empty() {
+                let next = self.next.take();
+                ret      = Some(self.take().extract_value());
+                *self    = next;
+            } else {
+                ret = Some(self.take().extract_value());
+            }
         }
         ret
     }
@@ -112,9 +115,19 @@ impl<T> LinkedList<T> {
             curr = &mut curr.next;
             i   += 1;
         }
-        let next  = curr.next.take();
-        node.next = next;
-        curr.next = node;
+        if !curr.is_empty() {
+            if index != 0 {
+                if !curr.next.is_empty() {
+                    node.next = curr.next.take();
+                }
+                curr.next = node;
+            } else {
+                node.next = curr.take();
+                *curr = node;
+            }
+        } else {
+            *curr = node;
+        }
     }
     pub fn remove(&mut self, index: usize) -> Option<T> {
         let mut ret  = None;
@@ -125,12 +138,19 @@ impl<T> LinkedList<T> {
             i   += 1;
         }
         if !curr.is_empty() {
-            let mut next = curr.next.take();
-            
-            if !next.next.is_empty() {
-                curr.next = next.next.take();
-                ret       = Some(next.extract_value());
-            } 
+            if index != 0 {
+                if !curr.next.is_empty() {
+                    let mut next = curr.next.take();
+                    if !next.next.is_empty() {
+                        curr.next = next.next.take();
+                    }
+                    ret = Some(next.extract_value());
+                }
+            } else {
+                let next = curr.next.take();                
+                ret      = Some(curr.take().extract_value());
+                *curr    = next;
+            }
         }
         ret
     }
@@ -152,21 +172,6 @@ impl<'a, T> Iterator for LinkedListIter<'a, T> {
 }
 
 
-pub struct LinkedListIterMut<'a, T>(&'a mut LinkedList<T>);
-
-impl<'a, T> Iterator for LinkedListIterMut<'a, T> {
-    type Item = &'a mut T;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.0.is_empty() {
-            None
-        } else {              
-            self.0  = &mut self.0.next;
-            let ret = Some(&mut self.0.value); 
-            ret
-        }
-    }
-}
-
 impl<'a, T> IntoIterator for &'a LinkedList<T> {
     type Item     = &'a T;
     type IntoIter = LinkedListIter<'a, T>;
@@ -174,14 +179,6 @@ impl<'a, T> IntoIterator for &'a LinkedList<T> {
         LinkedListIter(self)
     }
 }
-/*
-impl<'a, T> IntoIterator for &'a mut LinkedList<T> {
-    type Item     = &'a mut T;
-    type IntoIter = LinkedListIterMut<'a, T>;
-    fn into_iter(self) -> Self::IntoIter {
-        LinkedListIterMut(self)
-    }
-}*/
 
 impl<T> FromIterator<T> for LinkedList<T> {
     fn from_iter<I>(iter: I) -> Self 
@@ -196,12 +193,22 @@ impl<T> FromIterator<T> for LinkedList<T> {
     }
 }
 
+impl<T, I> From<I> for LinkedList<T> 
+where
+    I: Iterator<Item=T>,
+{
+    fn from(iter: I) -> Self {
+        iter.collect()
+    }
+}
+
 impl<T> Deref for LinkedList<T> {
     type Target = Node<T>;
     fn deref(&self) -> &Self::Target {
         match self {
             Filled(node) => node,
-            Empty => panic!("Attempt to dereference an empty list."),
+            Empty => panic!("Attempt to dereference 
+                             an empty list."),
         }
     }
 }
@@ -209,7 +216,8 @@ impl<T> DerefMut for LinkedList<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         match self {
             Filled(node) => node,
-            Empty => panic!("Attempt to dereference an empty list."),
+            Empty => panic!("Attempt to dereference 
+                             an empty list."),
         }
     }
 }
@@ -223,12 +231,16 @@ impl<T> Default for LinkedList<T> {
 impl<T> Index<usize> for LinkedList<T> {
     type Output = T;
     fn index(&self, i: usize) -> &Self::Output {
-        self.get(i).expect(&format!("Index value {} out of range.", i))
+        self.get(i)
+            .unwrap_or_else(|| panic!("Index value {} 
+                                       out of range.", i))
     }
 }
 impl<T> IndexMut<usize> for LinkedList<T> {
     fn index_mut(&mut self, i: usize) -> &mut Self::Output {
-        self.get_mut(i).expect(&format!("Index value {} out of range.", i))
+        self.get_mut(i)
+            .unwrap_or_else(|| panic!("Index value {}
+                                       out of range.", i))
     }
 }
 
@@ -241,12 +253,74 @@ mod tests {
         let list = (0..5).collect::<LinkedList<usize>>();
         assert_eq!(list.get(3).unwrap(), &3);
     }
-    
     #[test]
     fn remove() {
-        let mut list = (0..5).collect::<LinkedList<usize>>();
-        assert_eq!(list.remove(3).unwrap(), 3);
-        assert_eq!(list.get(3).unwrap(), &4);
+        let mut list = (0..=5).collect::<LinkedList<usize>>();
+        assert_eq!(list.remove(3), Some(3));
+        assert_eq!(list.get(3), Some(&4));
+        assert_eq!(list.remove(3), Some(4));
+        assert_eq!(list.remove(3), Some(5));
+        
+        list = (0..1).collect::<LinkedList<usize>>();
+        assert_eq!(list.remove(0), Some(0));
+        assert_eq!(list.remove(0), None);
+    }
+    #[test]
+    fn insert() {
+        let mut list = LinkedList::from(0..5);
+        list.insert(1, 9);
+        assert_eq!(list.get(1), Some(&9));
+        assert_eq!(list.get(2), Some(&1));
+        list = LinkedList::new();
+        list.insert(0, 9);
+        assert_eq!(list.get(0), Some(&9));
+        list.insert(0, 8);
+        assert_eq!(list.get(0), Some(&8));
+        assert_eq!(list.get(1), Some(&9));
+        assert_eq!(list.get(2), None);
+    }
+    #[test]
+    fn push_front() {
+        let mut list = LinkedList::new();
+        list.push_front(5);
+        assert_eq!(list.get(0), Some(&5));
+        list.push_front(4);
+        assert_eq!(list.get(0), Some(&4));
+        assert_eq!(list.get(1), Some(&5));
+        assert_eq!(list.get(2), None);
+    }
+    #[test]
+    fn push_back() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        assert_eq!(list.get(0), Some(&1));
+        assert_eq!(list.get(1), Some(&2));
+        assert_eq!(list.get(2), Some(&3));
+        assert_eq!(list.get(3), None);
+    }
+    #[test]
+    fn pop_front() {
+        let mut list = LinkedList::new();
+        assert_eq!(list.pop_front(), None);
+        list = LinkedList::from(0..3);
+        assert_eq!(list.pop_front(), Some(0));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), None);
+    }
+    #[test]
+    fn leet_1() {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        list.push_back(3);
+        list.insert(1, 2);
+        assert_eq!(list.get(0), Some(&1));
+        assert_eq!(list.get(1), Some(&2));
+        assert_eq!(list.get(2), Some(&3));
+        list.remove(0);
+        assert_eq!(list.get(0), Some(&2));
     }
 }
 
